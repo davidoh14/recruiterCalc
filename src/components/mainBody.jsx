ea44c234f41fbc9bd563406d2398359d3ea4ba3b
David Oh <86807281+davidoh14@users.noreply.github.com>
2022-03-07 02:56:10 -0600

    screenshot capture

--- src/components/mainBody.jsx
+++ src/components/mainBody.jsx
@@ -1,9 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, createRef} from "react";
 import Fields from "./fields";
 import Prompts from "./prompts";
+import {useScreenshot, createFileName} from 'use-react-screenshot'
+import html2canvas from 'html2canvas';
 
 const MainBody = () => {
     const [fieldCount, setFieldCount] = useState([1]);
+    const ref = createRef(null);
+    const [image, takeScreenShot] = useScreenshot({
+        type: "image/jpeg",
+        quality: 1.0
+    });
+
+    const download = (image, { name = "success-metrics-report", extension = "jpg" } = {}) => {
+        const a = document.createElement("a");
+        a.href = image;
+        a.download = createFileName(extension, name);
+        a.click();
+    };
+
+    const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
 
     function addQuarter() {
         let newFieldCount = [...fieldCount, (fieldCount.at(-1) + 1)]
@@ -16,16 +32,20 @@ const MainBody = () => {
     }
 
     return (
-        <div className="mainBody darkBlue df fdr">
-            <Prompts/>
-            {(fieldCount.length < 4) ? (<button className="addQ" onClick={() => addQuarter()}> + </button>) : null}
-            {(fieldCount.length > 1) ? (<button className="subQ" onClick={() => subQuarter()}> - </button>) : null}
-            <div className="fieldHalf df fdr">
-                {fieldCount.map(count => 
-                    <Fields count={count}/>    
-                )}
+        <div>
+            <button className="screenshot" onClick={downloadScreenshot}>Take Screenshot</button>
+            <div className="mainBody darkBlue df fdr" ref={ref}>
+                <Prompts/>
+                {(fieldCount.length < 4) ? (<button className="addQ" onClick={() => addQuarter()}> + </button>) : null}
+                {(fieldCount.length > 1) ? (<button className="subQ" onClick={() => subQuarter()}> - </button>) : null}
+                <div className="fieldHalf df fdr">
+                    {fieldCount.map(count => 
+                        <Fields count={count}/>    
+                    )}
+                </div>
             </div>
         </div>
+
     )
 }
 
